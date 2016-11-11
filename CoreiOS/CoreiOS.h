e0825b9e47ece8dca2934a20654baef35ae93c14
albinzr <albinzr@gmail.com>
2016-11-11 14:05:46 +0530

    Initial Commit

---
+++ CoreiOS/CoreiOS.h
@@ -0,0 +1,19 @@
+//
+//  CoreiOS.h
+//  CoreiOS
+//
+//  Created by Albin CR on 11/11/16.
+//  Copyright © 2016 Albin CR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreiOS.
+FOUNDATION_EXPORT double CoreiOSVersionNumber;
+
+//! Project version string for CoreiOS.
+FOUNDATION_EXPORT const unsigned char CoreiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreiOS/PublicHeader.h>
+
+
