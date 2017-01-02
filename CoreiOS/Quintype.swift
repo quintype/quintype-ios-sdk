66ff48b99961d80327c76d09de238459df4da048
albinzr <albinzr@gmail.com>
2017-01-02 15:29:39 +0530

    Release 0.0.10.

--- CoreiOS/Quintype.swift
+++ CoreiOS/Quintype.swift
@@ -7,7 +7,7 @@
 //
 
 protocol Completion{
-   func finished()
+    func finished()
 }
 
 
@@ -28,6 +28,7 @@ open class Quintype{
     private var _analytics:Analytics?
     private var _cache:Cache?
     private var _publisherConfig:Config?
+    private var _baseURL:String?
     
     //MARK: - Open variable for direct access - Api Services
     open static var api:ApiService{
@@ -39,32 +40,33 @@ open class Quintype{
         }
     }
     
-    //MARK: - Open variable for direct access - Analytic
-    open static var analytics:Analytics{
-        get{
-            if Quintype.sharedInstance._analytics == nil{
-                Quintype.sharedInstance._analytics = Analytics()
-            }
-            return Quintype.sharedInstance._analytics!
-        }
-    }
+    //    //MARK: - Open variable for direct access - Analytic
+    //    open static var analytics:Analytics{
+    //        get{
+    //            if Quintype.sharedInstance._analytics == nil{
+    //                Quintype.sharedInstance._analytics = Analytics()
+    //            }
+    //            return Quintype.sharedInstance._analytics!
+    //        }
+    //    }
+    //
+    //    //MARK: - PublisherConfig for direct acces - Config
+    //    open static var publisherConfig:Config{
+    //        get{
+    //            if Quintype.sharedInstance._publisherConfig == nil{
+    //
+    //                Quintype.sharedInstance.getPublisherConfig(options: publisherOption.all, success: { (data) in
+    //                Quintype.sharedInstance._publisherConfig = data as? Config
+    //                })
+    //            }
+    //            return Quintype.sharedInstance._publisherConfig!
+    //        }
+    //    }
+    //
     
-    //MARK: - PublisherConfig for direct acces - Config
-    open static var publisherConfig:Config{
-        get{
-            if Quintype.sharedInstance._publisherConfig == nil{
-                
-                Quintype.sharedInstance.getPublisherConfig(options: publisherOption.all, success: { (data) in
-                Quintype.sharedInstance._publisherConfig = data as? Config
-                })
-            }
-            return Quintype.sharedInstance._publisherConfig!
-        }
-    }
-    
-
     //MARK: - SDK init to obtain base url
     open static func initWithBaseUrl(baseURL: String!) {
+        Quintype.sharedInstance._baseURL = baseURL
         let defaults = UserDefaults.standard
         let storage = Storage.sharedStorage
         let regExp = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
@@ -94,46 +96,134 @@ open class Quintype{
             defaults.set(data?.razorpay_gateway_key, forKey: Constants.publisherConfig.razorpayKey)
             
             //calling analytic installation
-            let _ =  Quintype.analytics
-            let _ = Quintype.publisherConfig
+            //            let _ =  Quintype.analytics
+            //            let _ = Quintype.publisherConfig
+            //
             
+            //            if error == nil{
+            //                if let delegate = Quintype.sharedInstance.delegate{
+            //                    delegate.finished()
+            //                }
+            //            }
             
-            if error == nil{
-                if let delegate = Quintype.sharedInstance.delegate{
-                    delegate.finished()
+        }
+        
+    }
+    
+    //    //MARK - publisherConfig linking to Quintype
+    //
+    //
+    //        func getPublisherConfig(options:publisherOption,success:@escaping (Any?)->()){
+    //
+    //        Cache.retriveCacheData(keyName: Constants.publisherConfig.publisherCacheKey) { (data) in
+    //           //print(data as Any)
+    //
+    //            if data == nil{
+    //               //print("asdas")
+    //            }else{
+    //               //print("got data")
+    //                ApiParser.configParser(data: data as! [String : AnyObject]?, completion: { (configData) in
+    //
+    //                    if let opt = options.value{
+    //                        success(configData.value(forKey: opt.values.first!))
+    //                    }else{
+    //                        success(configData)
+    //                    }
+    //                })
+    //
+    //            }
+    //        }
+    //
+    //    }
+    
+    
+    
+    
+    
+    
+    //MARK: - Open variable for direct access - Analytic
+    open static var analytics:Analytics{
+        
+        get{
+            let defaults = UserDefaults.standard
+            if defaults.hasKey(Constants.publisherConfig.publisherKey){
+                if Quintype.sharedInstance._analytics == nil{
+                    Quintype.sharedInstance._analytics = Analytics()
+                }
+                return Quintype.sharedInstance._analytics!
+            }else{
+                Quintype.sharedInstance.LoadConfig(success: {
+                    if Quintype.sharedInstance._analytics == nil{
+                        Quintype.sharedInstance._analytics = Analytics()
+                    }
+                })
+                return Quintype.sharedInstance._analytics!
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private func LoadConfig(success:@escaping ()->()){
+        
+        
+        
+        if let _ = Quintype.sharedInstance._baseURL{
+            
+            Cache.retriveCacheData(keyName: Constants.publisherConfig.publisherCacheKey) { (data) in
+                //print(data as Any)
+                
+                if data == nil{
+                    //TODO: - ADD 10 sec delay
+                    print("failled.....10 sec delay")
+                    Quintype.sharedInstance.LoadConfig(success: {})
+                }else{
+                    ApiParser.configParser(data: data as! [String : AnyObject]?, completion: { (data) in
+                        
+                        let defaults = UserDefaults.standard
+                        defaults.set(data.publisher_id, forKey: Constants.publisherConfig.publisherKey)
+                        defaults.set(data.shrubbery_host, forKey: Constants.analyticConfig.analyticKey)
+                        defaults.set(data.publisher_name, forKey: Constants.publisherConfig.appNameKey)
+                        defaults.set(data.stripe_publishable_key, forKey: Constants.payment.stripeKey)
+                        defaults.set(data.cdn_name, forKey: Constants.publisherConfig.cdnNameKey)
+                        defaults.set(data.cdn_image, forKey: Constants.publisherConfig.cdnImageKey)
+                        defaults.set(data.sketches_host, forKey: Constants.publisherConfig.sketchesHostKey)
+                        defaults.set(data.num_headlines, forKey: Constants.publisherConfig.noOfStoriesOnTopKey)
+                        defaults.set(data.initial_stories_per_page, forKey: Constants.publisherConfig.noOfStoriesPerPageKey)
+                        defaults.set(data.typekit_id, forKey: Constants.publisherConfig.typekitKey)
+                        defaults.set(data.story_slug_format, forKey: Constants.publisherConfig.storySlugFormatKey)
+                        defaults.set(data.nudge_host, forKey:Constants.publisherConfig.nudgeHostKey)
+                        defaults.set(data.polltype_host, forKey: Constants.publisherConfig.polltypeHostKey)
+                        defaults.set(data.razorpay_gateway_key, forKey: Constants.publisherConfig.razorpayKey)
+                        defaults.set(data.razorpay_gateway_key, forKey: Constants.publisherConfig.razorpayKey)
+                        success()
+                        
+                    })
+                    
                 }
             }
             
-        }
-        
-    }
-    
-    //MARK - publisherConfig linking to Quintype
-    
-    
-        func getPublisherConfig(options:publisherOption,success:@escaping (Any?)->()){
-        
-        Cache.retriveCacheData(keyName: Constants.publisherConfig.publisherCacheKey) { (data) in
-           //print(data as Any)
             
-            if data == nil{
-               //print("asdas")
-            }else{
-               //print("got data")
-                ApiParser.configParser(data: data as! [String : AnyObject]?, completion: { (configData) in
-                    
-                    if let opt = options.value{
-                        success(configData.value(forKey: opt.values.first!))
-                    }else{
-                        success(configData)
-                    }
-                })
-                
-            }
+        }else{
+            print("no base URL.....10 sec delay")
+             Quintype.sharedInstance.LoadConfig(success: {})
         }
         
     }
-    
     
     
     
